06fd2365d5c708f335261ff270dd201cacf42e26
Megan Collier <meganraec@live.com>
2018-05-31 04:22:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
 
   def self.artist_name=(name)
     songs_artist = Artist.find_or_create_by_name(name)
-    name.artist = songs_artist
+    self.artist = songs_artist
   end
 
 #new_by_filename will strip ".mp3" from the filename, then parse it into its Artist and Song
