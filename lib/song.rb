ae443f0955d960e418f0a6918e3dbbf15b1062db
Megan Collier <meganraec@live.com>
2018-05-31 04:06:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,7 @@ class Song
     artistname = new_array[0]
     songname = new_array[1]
     instance = self.new(songname)
-    instance.artist_name=(artistname)
+    instance.artist = artistname
     binding.pry
     instance.artist.add_song(instance)
     instance
