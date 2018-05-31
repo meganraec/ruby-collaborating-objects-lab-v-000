9f5d6279ebaf925e221df278bb9d696ebbe927a9
Megan Collier <meganraec@live.com>
2018-05-31 03:31:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,10 +16,10 @@ class Song
 #new_by_filename will strip ".mp3" from the filename, then parse it into its Artist and Song
   def self.new_by_filename(file_name)
     new_array = file_name.split(" - ")
-    artistname = new_array[1]
-    songname = new_array[0]
+    artistname = new_array[0]
+    songname = new_array[1]
     new_instance = Song.new(songname)
-    new_instance.artist_name = artistname
+    new_instance.artist = artistname
     new_instance
     # song.artist = Artist.new(artistname)
     # song.artist.add_song(songname)
