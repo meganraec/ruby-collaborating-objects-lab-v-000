1ff01d8435912f620a08b9e488bd64513d8e470a
Megan Collier <meganraec@live.com>
2018-05-31 04:01:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,7 @@ class Song
     artistname = new_array[0]
     songname = new_array[1]
     instance = self.new(songname)
-    self.artist_name=(artistname)
+    instance.artist_name=(artistname)
     # instance.artist.add_song(instance)
     instance
   end
